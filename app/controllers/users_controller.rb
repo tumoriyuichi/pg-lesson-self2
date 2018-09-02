47ac3a64f909812c9b7ba8b039b7d8214be9e251
津森雄一 <>
2018-09-02 17:25:40 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+
+	def index
+		puts User.attribute_names
+
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(user_params)
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		user = User.find(params[:id])
+		user.update(user_params)
+	end
+
+	def destroy
+		user = User.find(params[:id])
+	user.destroy
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :age, :mail, :address, :gender, :program, :skils, :image)
+	end
+
+
+end
